20be7b6a8f0fde622159da498bde3890b1414734
Lucas Souza Gaudard da Silva <lucasgaudard07@gmail.com>
2025-12-12 22:19:14 -0300

    fix deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portif-lio/', // 👈 nome EXATO do repositório
+  base: '/portfolio/',
+  build: {
+    outDir: 'docs',
+  },
 })
